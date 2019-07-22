d3e0a41bf4b810bd727e9abccc2335a9c702f7d4
Osmond Jiang <xinjiang@microsoft.com>
2019-07-22 22:19:59 +0800

    create proxy between rendering and hosting

--- src/docfx/watch/Host.cs
+++ src/docfx/watch/Host.cs
@@ -2,31 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Docs.Build
 {
     internal static class Host
     {
-        private static string _docset;
-        private static string _baseUrl;
-        private static string _baseFolder;
-        private static ErrorLog _errorLog;
-
-        public static void Config(string docset, Config config, ErrorLog errorLog)
-        {
-            _docset = docset;
-            _baseUrl = config.BaseUrl;
-            _baseFolder = config.DocumentId.SourceBasePath;
-            _errorLog = errorLog;
-        }
-
-        public static void Run()
+        public static Task<int> CreateHostWebService(string docset, string baseUrl, int port)
         {
             // create depots based on current config
             var depotsFilePath = "./Database/storage.json";
@@ -38,10 +24,10 @@ namespace Microsoft.Docs.Build
 
             var depot = new
             {
-                LocalPath = Path.GetFullPath(_docset),
+                LocalPath = Path.GetFullPath(docset),
                 DepotId = Guid.NewGuid().ToString(),
                 DepotName = "test",
-                SiteBasePath = _baseUrl,
+                SiteBasePath = baseUrl,
                 SiteId = Guid.NewGuid().ToString(),
                 PartitionNumber = 0,
                 Metadata = new
@@ -55,48 +41,21 @@ namespace Microsoft.Docs.Build
 
             File.WriteAllText(depotsFilePath, JsonConvert.SerializeObject(new { Depots = new[] { depot } }));
 
-            // start dhs emulator
-            DocumentHostingEmulator.Program.Run<HostStartup>();
-        }
+            // create host file
+            var hostFile = "hosting.json";
 
-        private class HostStartup : DocumentHostingEmulator.Startup
-        {
-            public HostStartup(IConfiguration configuration)
-                : base(configuration)
-            {
-            }
+            File.WriteAllText(hostFile, JsonConvert.SerializeObject(new JObject { ["server.urls"] = $"http://localhost:{port}" }));
 
-            public new void Configure(IApplicationBuilder app, IHostingEnvironment env)
-            {
-                if (env.IsDevelopment())
-                {
-                    app.UseBrowserLink();
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                }
+            // create dhs emulator process
+            var serverDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../dep/OpenPublishing.DocumentHostingEmulator/Source/DocumentHostingEmulator/DocumentHostingEmulator"));
+            var serverArgs = "run --no-launch-profile --no-build --no-restore";
+            var psi = new ProcessStartInfo { FileName = "dotnet", WorkingDirectory = serverDir, Arguments = serverArgs };
 
-                app.UseStaticFiles();
-
-                app.Use(next => async http =>
-                {
-                    if (http.Request.Path.StartsWithSegments("/" + _baseUrl, out var remainingPath))
-                    {
-                        var sourceFilePath = _baseFolder + remainingPath.Value;
-                        await Build.Run(_docset, null, _errorLog, new List<string> { sourceFilePath });
-                        await next(http);
-                    }
-                });
-
-                app.UseMvc(routes =>
-                {
-                    routes.MapRoute(
-                        name: "default",
-                        template: "{controller=Home}/{action=Index}/{id?}");
-                });
-            }
+            var tcs = new TaskCompletionSource<int>();
+            var process = Process.Start(psi);
+            process.EnableRaisingEvents = true;
+            process.Exited += (a, b) => tcs.TrySetResult(process.ExitCode);
+            return tcs.Task;
         }
     }
 }
--- src/docfx/watch/Watch.cs
+++ src/docfx/watch/Watch.cs
@@ -2,10 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.Docs.Build
 {
@@ -16,24 +22,70 @@ namespace Microsoft.Docs.Build
             var (_, config) = ConfigLoader.Load(docsetPath, options);
 
             // start hosting via dhs emulator
-            Host.Config(docsetPath, config, errorLog);
-            Host.Run();
+            Host.CreateHostWebService(docsetPath, config.BaseUrl, 5000).Start();
 
-            // lanch rending site
-            await LaunchWebServer();
+            // creat host proxy
+            CreateHostProxy(docsetPath, config.BaseUrl, config.DocumentId.SourceBasePath, options, errorLog).Start();
+
+            // luanch docs rending site
+            await CreateRenderingServicer();
         }
 
-        private static Task<int> LaunchWebServer()
+        private static IWebHost CreateHostProxy(string docset, string siteBasePath, string sourceBasePath, CommandLineOptions options, ErrorLog errorLog)
+        {
+            var httpClient = new HttpClient()
+            {
+                // dhs emulator
+                BaseAddress = new Uri("http://localehost:5000"),
+            };
+
+            return new WebHostBuilder()
+                .UseUrls($"http://*:{options.Port}")
+                .Configure(Configure)
+                .Build();
+
+            void Configure(IApplicationBuilder app)
+            {
+                app.Use(next => async http =>
+                {
+                    if (http.Request.Path.StartsWithSegments("/" + siteBasePath, out var remainingPath))
+                    {
+                        var filePath = Path.Combine(sourceBasePath, remainingPath.Value);
+
+                        try
+                        {
+                            // build file before go to dhs emulator
+                            await Build.Run(docset, options, errorLog, new List<string> { filePath });
+                            var request = new HttpRequestMessage
+                            {
+                                Method = HttpMethod.Get,
+                                RequestUri = new Uri(http.Request.Path, UriKind.Relative),
+                            };
+
+                            using (var response = await httpClient.SendAsync(request))
+                            {
+                                http.Response.StatusCode = (int)response.StatusCode;
+                                await response.Content.CopyToAsync(http.Response.Body);
+                            }
+
+                            await next(http);
+                        }
+                        catch
+                        {
+                            http.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            await http.Response.WriteAsync("build failed, check the build log");
+                        }
+                    }
+                });
+            }
+        }
+
+        private static Task<int> CreateRenderingServicer()
         {
             var port = 56789;
-            var name = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "App.exe" : "App";
-            var exeDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../web"));
-            var exe = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../web", name));
             var serverDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../dep/Docs.Rendering/Source/App"));
             var serverArgs = "run --no-launch-profile --no-build --no-restore";
-            var psi = File.Exists(exe)
-                ? new ProcessStartInfo { FileName = exe, WorkingDirectory = exeDir, }
-                : new ProcessStartInfo { FileName = "dotnet", WorkingDirectory = serverDir, Arguments = serverArgs };
+            var psi = new ProcessStartInfo { FileName = "dotnet", WorkingDirectory = serverDir, Arguments = serverArgs };
 
             psi.UseShellExecute = false;
             psi.EnvironmentVariables["ASPNETCORE_ENVIRONMENT"] = "Development";
